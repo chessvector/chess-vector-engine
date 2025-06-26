88de02c75631afd511093455023876b4f004e296
speroleague <speroleague@users.noreply.github.com>
2025-06-26 19:33:23 +0000

    deploy: 7854ba70966a6a754130a25c7a84f1fa5dbcb0f6

--- trait.impl/core/marker/trait.Copy.js
+++ trait.impl/core/marker/trait.Copy.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["chess_vector_engine",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.87.0/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"chess_vector_engine/ann/enum.SearchStrategy.html\" title=\"enum chess_vector_engine::ann::SearchStrategy\">SearchStrategy</a>"]]]]);
+    var implementors = Object.fromEntries([["chess_vector_engine",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.88.0/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"chess_vector_engine/ann/enum.SearchStrategy.html\" title=\"enum chess_vector_engine::ann::SearchStrategy\">SearchStrategy</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
